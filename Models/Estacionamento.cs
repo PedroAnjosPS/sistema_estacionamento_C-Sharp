1bd87f235b5531bb00c54468ec4e1c5049cad2cf
PedroAnjosPS <phpsanches@gmail.com>
2026-05-06 00:03:53 -0300

    Incorporando a estrutura principal do programa, dos métodos de Adicionar e Listar veículos

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -10,6 +10,60 @@ namespace sistemaEstacionamento_C_Sharp.Models
     /// </summary>
     public class Estacionamento
     {
-        
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            string veiculo = "";
+
+            do
+            {
+                Console.Clear();
+                Console.Write("\nDigite a placa do veículo para estacionar: ");
+                veiculo = Console.ReadLine();
+
+                if (veiculo.Length != 7 || !veiculo.Contains("-"))
+                {
+                    Console.WriteLine("Formato de placa de veículo incorreto! Digite novamente!");
+                    Thread.Sleep(1000);
+                }
+            } while (veiculo.Length != 7 || !veiculo.Contains("-"));
+
+            veiculos.Add(veiculo);
+
+            Console.WriteLine("Veículo estacionado com sucesso!");
+        }
+
+        public void RemoverVeiculo()
+        {
+            veiculos.Remove("veiculo");
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("\nVeículos estacionados:");
+                int i = 1;
+
+                foreach (string veic in veiculos)
+                {
+                    Console.WriteLine(i + veic);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNão há veículos estacionados.");
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿using sistemaEstacionamento_C_Sharp.Models;
 
-int opcao = 0;
+// Coloca o enconding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.Write("\n======== BEM VINDO AO SISTEMA DE ESTACIONAMENTO! ========\n"
+                + "\nDigite o preço inicial: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Agora digite o preço por hora: ");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento já com os valores inseridos anteriormente
+Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+string opcao = string.Empty;
+bool exibirMenu = true;
 
 do
 {
+    Console.Clear();
     Console.Write("Digite a sua opção: " +
             "\n1 - Cadastrar veículo" +
             "\n2 - Remover veículo" +
@@ -11,7 +28,37 @@ do
             "\n4 - Encerrar" +
             "\nResposta: ");
 
-    opcao = int.Parse(Console.ReadLine());
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            estacionamento.AdicionarVeiculo();
+            Thread.Sleep(1700);
+            break;
+
+        case "2":
+            estacionamento.RemoverVeiculo();
+            Thread.Sleep(1700);
+            break;
+
+        case "3":
+            estacionamento.ListarVeiculos();
+            Console.Write("\nPressione qualquer tecla para prosseguir a execução do programa:");
+            opcao = Console.ReadLine();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            Console.WriteLine("\nSaindo do sistema...");
+            Thread.Sleep(1700);
+            break;
+
+        default:
+            Console.WriteLine("\nOpção inválida!");
+            Thread.Sleep(1700);
+            break;    
+    }
 
 
-} while (true);
+} while (exibirMenu);
