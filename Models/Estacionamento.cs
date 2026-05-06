9255f6e1a3661ac4dae99f42547b6af08fe5aef1
PedroAnjosPS <phpsanches@gmail.com>
2026-05-05 22:55:03 -0300

    Primeiro commit

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistemaEstacionamento_C_Sharp.Models
+{
+    /// <summary>
+    /// Representa um modelo de estacionamento da vida real
+    /// </summary>
+    public class Estacionamento
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using sistemaEstacionamento_C_Sharp.Models;
+
+int opcao = 0;
+
+do
+{
+    Console.Write("Digite a sua opção: " +
+            "\n1 - Cadastrar veículo" +
+            "\n2 - Remover veículo" +
+            "\n3 - Listar veículos" +
+            "\n4 - Encerrar" +
+            "\nResposta: ");
+
+    opcao = int.Parse(Console.ReadLine());
+
+
+} while (true);
